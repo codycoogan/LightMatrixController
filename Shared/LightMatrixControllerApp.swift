31ff2f0ba6258c0279d18f9eb8a29e357f21eb2d
Cody Coogan <codycoogan@gmail.com>
2022-06-24 09:35:21 -0500

    Initial Commit

---
+++ Shared/LightMatrixControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightMatrixControllerApp.swift
+//  Shared
+//
+//  Created by Cody Coogan on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct LightMatrixControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
